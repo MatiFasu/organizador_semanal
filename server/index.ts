fe7105441449099cbaaf5e8d48a20bd4430ff23f
MatiFasu <matiasfasulino@gmail.com>
2026-05-04 10:37:13 -0300

    Fix Express 5 catch-all route using RegEx for compatibility

--- server/index.ts
+++ server/index.ts
@@ -221,7 +221,8 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('/:path*', (req, res) => {
+// Using regex to avoid path-to-regexp v8 naming issues.
+app.get(/^(?!\/api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
